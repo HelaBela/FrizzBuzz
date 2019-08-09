d1036f33d23c27311b6e03f6084418961d00f344
Helena Franczak <helena.franczak@myob.com>
2019-08-09 16:09:36 +1000

    improved code, method added

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,29 +4,36 @@ namespace HelloWorld
 {
     class Program
     {
+        static string GetResult(int i)
+        {
+            var isDivisibleByThree = i % 3 == 0;
+            var isDivisibleByFive = i % 5 == 0;
+            var result = "";
+            
+            if (isDivisibleByFive && isDivisibleByThree)
+            {
+                result = "FrizzBuzz";
+            }else if (isDivisibleByThree)
+            {
+                result = "Frizz";
+            } else if (isDivisibleByFive)
+            {
+                result = "Buzz";
+            }
+            
+            else
+            {
+                result = i.ToString();
+            }
+            return result;
+        }
+
         static void Main(string[] args)
         {
+            
             for (int i = 1; i <= 100; i++)
             {
-                var isDivisibleByThree = i % 3 == 0;
-                var isDivisibleByFive = i % 5 == 0;
-
-                if (isDivisibleByThree)
-                {
-                    Console.Write("Frizz");
-                }
-
-                if (isDivisibleByFive)
-                {
-                    Console.Write("Buzz");
-                }
-
-                if (isDivisibleByFive == false && isDivisibleByThree == false)
-                {
-                    Console.Write(i);
-                }
-
-                Console.WriteLine();
+                Console.WriteLine(GetResult(i));
             }
         }
     }
